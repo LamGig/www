03e77d73eccdefad4dd28fdad158ced086ab3f0b
Nhat Nguyen <nhat@thepixelme.com>
2025-07-10 01:38:59 -0400

    add Site Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { SiteHeader } from "@/components/SiteHeader";
 import { HeroSection } from "@/components/HeroSection";
+import { SiteFooter } from "@/components/SiteFooter";
 
 export default function Home() {
   return (
     <>
       <SiteHeader />
       <HeroSection />
+      <SiteFooter />
     </>
   );
 }
