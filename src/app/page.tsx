aaec42fa41bd5b7d54b6da20c8e02a433ad9bea3
Nhat Nguyen <nhat@thepixelme.com>
2025-08-06 22:42:36 -0400

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { SiteHeader } from "@/components/SiteHeader";
+import { HeroSection } from "@/components/HeroSection";
 import { SiteFooter } from "@/components/SiteFooter";
 
 export default function Home() {
   return (
     <>
       <SiteHeader />
+      <HeroSection />
       <SiteFooter />
     </>
   );
---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export const HeroSection = () => {
+  return (
+    <section>
+      
+    </section>
+  );
+};
